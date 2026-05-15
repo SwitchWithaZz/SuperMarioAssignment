4c7b26098664eb514b19968b448437fb0eba4a46
Markus Tyler <markuskt@uia.no>
2026-05-15 03:53:50 +0200

    a

--- game/include/core/Game.h
+++ game/include/core/Game.h
@@ -2,12 +2,13 @@
 
 #include <SFML/Graphics.hpp>
 #include "entities/Player.h"
+#include "world/Level.h"
 
 class Game {
 private:
     sf::RenderWindow window;
     Player player;
-
+    Level level;
 
     void processEvents();
     void update(float dt);
