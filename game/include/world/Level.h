7b7e93b61dec824b56217e7460c81e4eee710b50
Gabriel Lande <gabrielfl@uia.no>
2026-05-15 09:31:03 +0200

    added mushroom sprite

--- game/include/world/Level.h
+++ game/include/world/Level.h
@@ -8,6 +8,21 @@
 #include "../entities/Coin.h"
 #include "../entities/Mushroom.h"
 
+const sf::Texture groundTexture("../assets/textures/Blocks/SMB_ground.png");
+inline sf::Sprite ground(groundTexture);
+
+const sf::Texture brickTexture("../assets/textures/Blocks/SMB_Brick_block_Sprite.png");
+inline sf::Sprite brick(brickTexture);
+
+const sf::Texture qBTexture("../assets/textures/Blocks/SMB_Qblock.png");
+inline sf::Sprite qB(qBTexture);
+
+const sf::Texture emptyBrickTexture("../assets/textures/Blocks/SMB1_Empty_block.png");
+inline sf::Sprite emptyBrick(emptyBrickTexture);
+
+const sf::Texture coinTexture("../assets/textures/Blocks/SMB_Sprite_Coin.png");
+inline sf::Sprite coin(coinTexture);
+
 class Level {
 private:
     std::vector<std::string> map;
